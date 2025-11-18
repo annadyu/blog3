c6c64cf31253b8769423cfb44d6d075edb99005b
dyuanna <annadyu666@gmail.com>
2025-11-18 19:48:19 +0900

    ccc

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-   base: "/blog3/",
+   base: "/",
   plugins: [react()],
   server: {
     proxy: {
